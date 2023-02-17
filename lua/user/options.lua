d02daebf3bec462aadbb0119bbea14c5e5316e97
辰世 <chenshi.ck@alibaba-inc.com>
2023-02-17 17:27:05 +0800

    fist init , add base config

---
+++ lua/user/options.lua
@@ -0,0 +1,24 @@
+-- vim options
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.relativenumber = true
+vim.opt.showmode = true
+vim.opt.cmdheight = 1
+vim.opt.spelllang = "en"
+
+-- vim.opt.foldcolumn = '0'
+-- vim.opt.foldenable = true
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldlevel = 99
+-- vim.opt.foldlevelstart = 99
+
+--lvim options
+lvim.log.level = "info"
+lvim.format_on_save = { enabled = false }
+-- lvim.format_on_save = {
+--     enabled = true,
+--     pattern = "*.lua",
+--     timeout = 1000,
+-- }
+
---
+++ lua/user/plugins.lua
@@ -0,0 +1,61 @@
+-- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
+-- lvim.plugins = {
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
+
+local plugins = {
+    -- { "Exafunction/codeium.vim" },
+    {
+        "Pocco81/auto-save.nvim",
+    },
+    {
+        "ethanholz/nvim-lastplace",
+    },
+    {
+        "xiyaowong/nvim-transparent",
+    },
+    {
+        "phaazon/hop.nvim",
+        event = "BufRead",
+        config = function()
+          require("hop").setup()
+        end,
+    },
+    {
+        'kevinhwang91/nvim-hlslens',
+    },
+    {
+        "ray-x/go.nvim",
+        dependencies = { -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+          require("go").setup()
+        end,
+        event = { "CmdlineEnter" },
+        ft = { "go", 'gomod' },
+        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    }
+}
+
+
+local colorschemes = {
+    -- { "luisiacc/gruvbox-baby" },
+    -- { "nyoom-engineering/oxocarbon.nvim" },
+    { "catppuccin/nvim" },
+    -- { "EdenEast/nightfox.nvim" },
+    -- { "marko-cerovac/material.nvim" },
+}
+
+for _, value in ipairs(colorschemes) do
+  table.insert(plugins, value)
+end
+
+-- require("lvim-user.plugins.nvim-ide")
+
+return plugins
