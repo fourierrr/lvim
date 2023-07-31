2ffd5aa94df33119811e870e4088bbcca76a1341
辰世 <chenshi.ck@alibaba-inc.com>
2023-07-31 16:39:34 +0800

    first commit

---
+++ lua/user/conf/init.lua
@@ -0,0 +1,37 @@
+local ends_with = function(str, ending)
+  return ending == "" or str:sub( -#ending) == ending
+end
+
+
+local M = {}
+
+-- 创建一个命令以触发该函数
+
+M.setup = function()
+  -- local config_dir = vim.fn.stdpath('config') .. '/lua/user/conf'
+  local config_dir = '~/.config/lvim/lua/user/conf'
+  -- plugins do not need to load, NOTE: no .lua suffix required
+  local unload_plugins = {
+      "init", -- we don't need to load init again
+  }
+
+  local helper_set = {}
+  for _, v in pairs(unload_plugins) do
+    helper_set[v] = true
+  end
+  local path = vim.fn.expand(config_dir)
+  for _, fname in pairs(vim.fn.readdir(path)) do
+    if ends_with(fname, ".lua") then
+      local cut_suffix_fname = fname:sub(1, #fname - #'.lua')
+      if helper_set[cut_suffix_fname] == nil then
+        local file = "user.conf." .. cut_suffix_fname
+        local status_ok, _ = pcall(require, file)
+        if not status_ok then
+          vim.notify('Failed loading ' .. fname, vim.log.levels.ERROR)
+        end
+      end
+    end
+  end
+end
+
+M.setup()
---
+++ lua/user/plugins.lua
@@ -0,0 +1,89 @@
+-- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
+-- lvim.plugins = {
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
+
+local plugins = {
+  -- { "Exafunction/codeium.vim" },
+  {
+    "Pocco81/auto-save.nvim",
+  },
+  -- {
+  --   "github/copilot.vim",
+  -- },
+  {
+    'michaelb/sniprun',
+    build = 'bash ./install.sh',
+    event = "BufRead",
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+  {
+    "ethanholz/nvim-lastplace",
+  },
+  {
+    "xiyaowong/nvim-transparent",
+  },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+    end,
+  },
+  {
+    'kevinhwang91/nvim-hlslens',
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+  },
+  {
+    "kkharji/lspsaga.nvim",
+    dependencies = { { "nvim-tree/nvim-web-devicons" } }
+  },
+
+}
+
+
+local colorschemes = {
+  -- { "luisiacc/gruvbox-baby" },
+  -- { "nyoom-engineering/oxocarbon.nvim" },
+  { "catppuccin/nvim" },
+  -- { "EdenEast/nightfox.nvim" },
+  -- { "marko-cerovac/material.nvim" },
+}
+
+for _, value in ipairs(colorschemes) do
+  table.insert(plugins, value)
+end
+
+-- require("lvim-user.plugins.nvim-ide")
+
+return plugins
